5302cc5568447d20428530c795ee3358c2942c4e
xavier <breuilxavier@gmail.com>
2024-09-06 08:44:05 +0200

    dev: Allow request from dev front.

--- D2D_guide_backend/D2D_guide_backend/settings.py
+++ D2D_guide_backend/D2D_guide_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Third party apps
     'rest_framework',
+    'corsheaders',
 
     # Custom app
     'task'
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,3 +137,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+# Allow request from development front
+# TODO: Set it up properly when configuring for prod
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
