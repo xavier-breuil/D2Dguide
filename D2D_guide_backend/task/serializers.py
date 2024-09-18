4fd6254852d26e655f7dc37980d00b5d44a27212
xavier <breuilxavier@gmail.com>
2024-09-18 16:50:14 +0200

    dev: Add id in tasks serializers.

--- D2D_guide_backend/task/serializers.py
+++ D2D_guide_backend/task/serializers.py
@@ -9,10 +9,10 @@ from task.models import DatedTask, WeekTask
 class DatedTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = DatedTask
-        fields = ['name', 'date', 'done']
+        fields = ['name', 'date', 'done', 'id']
 
 
 class WeekTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeekTask
-        fields = ['name', 'week_number', 'done']
+        fields = ['name', 'week_number', 'done', 'id']
